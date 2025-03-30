176fe474d792f706a963f42c71b9d577cfc8a495
MayaP3 <mpeacock@ramapo.edu>
2025-03-30 17:21:05 -0400

    fix: display_data now can take in a filepath and convert it to satisfy the uniccode requirement by converting \ to / and removing quotations

--- database_normalizer.py
+++ database_normalizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 # Import csv
 def display_data(csv_path):
+    csv_path = csv_path.replace('"', "").replace("\\", "/")
     data = pd.read_csv(csv_path)
     df = pd.DataFrame(data)
     print(df)
