2dfc86f4a4f108ad17c332753747b3bdfa0f0207
MayaP3 <mpeacock@ramapo.edu>
2025-03-30 16:17:56 -0400

    feature: created a display_data function that currently takes a csv file path, converts it to a pd df and prints it

---
+++ database_normalizer.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+# Import csv
+def display_data(csv_path):
+    data = pd.read_csv(csv_path)
+    df = pd.DataFrame(data)
+    print(df)
+
+
+def main():
+
+    csv_path = input("Give path to csv file: ")
+    display_data(csv_path)
