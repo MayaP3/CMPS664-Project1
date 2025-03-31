02a65ac11afd170cb40bfd2f6015cdb368be6382
MayaP3 <mpeacock@ramapo.edu>
2025-03-30 21:37:18 -0400

    feature: created function database_fds that takes the functional dependencies and prints a list of the determinants, the dependents and a dict of the FDs

--- database_normalizer.py
+++ database_normalizer.py
@@ -11,10 +11,37 @@ def display_data(csv_path):
     print(df.dtypes)
 
 
+# Functional Dependency Identifcation
+def database_fds(functional_dependenices: list, primary_keys: list):
+
+    determinants = []
+    dependents = []
+
+    FD_dict = {}
+
+    for i in range(len(functional_dependenices)):
+        left, right = functional_dependenices[i].split('->')
+        determinants.append(left)
+        dependents.append(right)
+        FD_dict[left] = right
+
+    print("Deteminants: ", determinants)
+    print("")
+    print("Dependents: ", dependents)
+    print("")
+    print("Functional Dependencies: ", FD_dict)
+
+
 def main():
 
     csv_path = input("Give path to csv file: ")
     display_data(csv_path)
+
+    # relation_name = input("Enter relation name: ")
+    functional_dependencies = list(input("Enter functional dependencies (e.g., A->B, C->D): ").split(','))
+    primary_keys = list(input("Enter primary key(s): ").split(','))
+
+    database_fds(functional_dependencies, primary_keys)
 
 
 if __name__ == '__main__':
