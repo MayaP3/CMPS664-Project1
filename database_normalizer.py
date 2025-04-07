1d8e6f7877dcb53a8198a36693ff7a3b11e658e3
MayaP3 <mpeacock@ramapo.edu>
2025-04-06 20:52:24 -0400

    style: Added docstrings and comments to functions for better understanding of each function

--- database_normalizer.py
+++ database_normalizer.py
@@ -2,8 +2,11 @@ import pandas as pd
 import itertools
 
 
-# Import csv
 def display_data(csv_path):
+    """Takes in csv path, cleans the input by removing '"' and replacing '\'
+    with '/'. Then reads in the csv, printing a sample of the dataframe and
+    the datae types ofeach columns."""
+
     csv_path = csv_path.replace('"', "").replace("\\", "/")
     data = pd.read_csv(csv_path)
     df = pd.DataFrame(data)
@@ -14,8 +17,14 @@ def display_data(csv_path):
     return df
 
 
-# Functional Dependency Identifcation
 def database_fds(functional_dependencies: list):
+
+    """
+    Analyzes a list of functional dependencies given as
+    (i.e. ["A->B,C", "B->C"]) and extracts the relation, determinants,
+    dependents, and the list of functional dependencies in the form
+    of a list of tuples.
+    """
 
     relation = set()
     determinants = []
@@ -24,20 +33,30 @@ def database_fds(functional_dependencies: list):
     FD_list = []
 
     for i in range(len(functional_dependencies)):
+        # Split each FD on '->' (i.e "A|B->C,D" --> (A|B), (C,D))
         left, right = functional_dependencies[i].split('->')
 
+        # Remove spaces and split right into a list on ',' (i.e. r = [C, D])
         right = [item.replace(" ", "") for item in right.split(',')]
 
+        # For every dependent per determinant, add the deter x times to
+        # the deter list
         for i in range(len(right)):
             determinants.append(left)
 
+        # Add each dependent to dependent list
         for dependent in right:
             dependents.append(dependent)
 
+        # Add a tuple of the deter and the dep to the FD_list st. dep is a
+        # list of all the deps the deter has
         FD_list.append((left, right))
 
+        # Update the set (for uniqueness) of the relation by adding the
+        # deter to it. Spliting by '|' if needed
         relation.update(list(left.split('|')))
 
+        # Update the relation by adding the dependent to it
         for r in right:
             relation.update(list({r}))
 
@@ -49,63 +68,109 @@ def database_fds(functional_dependencies: list):
     print("")
     print("Functional Dependencies: ", FD_list)
 
+    # Relation is a set, deter and deps as a list and FD_List is a list
+    # of tuples. The first index being the deter and the second being
+    # a list of all the deps the deter leads to
     return relation, determinants, dependents, FD_list
 
 
 def attribute_combos(determinants: list, dependents: list):
 
+    """
+    Helper function for compute_closure that creates a list of every
+    possible combo between the deters and the deps, dividing each
+    combo with a '/'
+
+    i.e Deter = [A,B] Deps = [C,D]
+    Combos =  ['A', 'B', 'C', 'D', 'A/B', 'A/C', 'A/D', 'B/C', 'B/D',
+    'C/D', 'A/B/C', 'A/B/D', 'A/C/D', 'B/C/D', 'A/B/C/D']
+    """
+
+    # Combine the two lists (unique)
     for dep in dependents:
         combined_list = list(set(determinants + dep))
 
     combinations = []
 
-    for r in range(1, len(combined_list) + 1):
-        for combo in itertools.combinations(combined_list, r):
+    # Generate combinations of different lengths (1 to the length of
+    # the combined list)
+    for combo_size in range(1, len(combined_list) + 1):
+        # combo is a tuple of attrs based on the combo size.
+        # combined_list = ['A', 'B', 'C'], combo_size = 2
+        # combo will be one of: [('A', 'B'), ('A', 'C'), ('B', 'C')]
+        for combo in itertools.combinations(combined_list, combo_size):
+            # combo ('A', 'B') becomes "A/B"
             combinations.append('/'.join(combo))
 
     return combinations
 
 
 def compute_closure(FD_list: list):
+    """
+    Computes closure of every attribute and attribute combo give a list of
+    tuples of the FDs
+    i.e. FD_list = [('StudID|CourID', ['StudName', 'CourName', 'InstrName']),
+            ('StudID', ['StudName']), ('CourID', ['CourName', 'InstrName'])]
+    """
+
     closure_dict = {}
     determinants = []
     dependents = []
 
+    # Seperate the FDs into determinants and dependents
     for determinant, dependent in FD_list:
         determinants.append(determinant)
         dependents.append(dependent)
 
+    # Adding determinants into closure dict
     for determinant, dependent in FD_list:
         if determinant not in closure_dict.keys():
+            # Determinant is a composition of two attrs, split via '|'
             composite = tuple(determinant.split('|'))
             closure_dict[determinant] = set()
+            # if deter = A|B, closure of A|B should start with {A,B}
             for c in composite:
                 closure_dict[determinant].add(c)
+                # Closure of A and B seperately should include themselves
                 if c not in closure_dict:
                     closure_dict[c] = {c}
 
-    for dep in dependent:
-        closure_dict[determinant].update({dep})
-
-    for dep in dependent:
-        if dep not in closure_dict.keys():
-            closure_dict[dep] = {dep}
-
-    if determinant in closure_dict.keys():
+        # Adding Dependents to closure set of determinants
         for dep in dependent:
-            closure_dict[dep].update({dep})
+            closure_dict[determinant].update({dep})
 
+        # Closure of a Dependent is itself
+        for dep in dependent:
+            if dep not in closure_dict.keys():
+                closure_dict[dep] = {dep}
+
+        # if determinant is already in closure.keys, add it's dep to its
+        # closure set
+        if determinant in closure_dict.keys():
+            for dep in dependent:
+                closure_dict[dep].update({dep})
+
+    # If any determinant's dependent is also a determinant (Transitive),
+    # add the dependent's dependents to the original deter's closure set
+    # i.e A->B, C; B->D ---> A+ = [A, B, C, D]
     for determinant in closure_dict.keys():
         for key, value in closure_dict.items():
             if determinant in value:
                 closure_dict[key].update(closure_dict[determinant])
 
+    # attribute_combos_results is a list of every combo between the deters
+    # and deps
+    # ['A', 'B', 'C', 'A|B', 'A/B', 'A/B', 'A/C', 'A/A|B', ..., 'A/B/C/A|B']
     attribute_combos_results = attribute_combos(determinants, dependents)
     print(attribute_combos_results)
 
+    # Finding closure of every possible attr combo
     for attr_combo in attribute_combos_results:
         if attr_combo not in closure_dict.keys():
             closure_dict[attr_combo] = set()
+            # Seperate the combos by '/' and combine their individual closure
+            # sets into
+            # a unique set of their closures
             attrs = [attrs for attrs in attr_combo.split('/')]
             for attr in attrs:
                 closure_dict[attr_combo].update(closure_dict[attr])
@@ -116,13 +181,28 @@ def compute_closure(FD_list: list):
     return closure_dict
 
 
-def find_partial_dependencies(primary_keys: list, closure_dict: dict) -> dict:
+def find_partial_dependencies(primary_keys: list, closure_dict: dict) -> list:
+    """
+    Creates a list of partial dependents given a list of the primary keys and
+    a complete closure dict of every attribute and attribute combo's closure
+    set
+    """
+
     possible_partial_dependencies = {}
 
+    # partial_deps only occur if the primary key is a composite key
     if len(primary_keys) > 1:
+        # Comparing each primary key
         for i in primary_keys:
             for j in primary_keys:
+                # If their closure sets aren't the same, hence they're
+                # differnt attrs with different deps despite being a primary
+                # key
                 if closure_dict[i] != closure_dict[j]:
+                    # For all the denpendencies from primary key i that aren't
+                    # a dep of primary key j, add that dependency to
+                    # possible_partial_deps as the value with primary key i as
+                    # the key
                     for dependency in closure_dict[i]:
                         if (dependency not in closure_dict[j]) & (i != dependency):
                             if i not in possible_partial_dependencies:
@@ -133,40 +213,79 @@ def find_partial_dependencies(primary_keys: list, closure_dict: dict) -> dict:
     print("")
     print("With primary keys: ", primary_keys)
     print("Possible Partial Dependencies: ", possible_partial_dependencies)
+
+    # If the possible_partial_dependencies list length is > 0, a partial_dep
+    # was found and hence the dataframe does not satisfy 2NF
     if len(possible_partial_dependencies) > 0:
         satisfies_2nf = False
     else:
         satisfies_2nf = True
+
     return satisfies_2nf, possible_partial_dependencies
 
 
 def find_transitive_dependencies(closure_dict: dict, determinants: list, FD_list: list):
-    transitive_dependencies = {}
+    """
+    Creates a list of the transitive_dependencies
+    (i.e A->B, B->C ---> [(A, C)]) given the closure dictionary of every
+    attribute and attribute combo's closure sets, a list of the
+    determinants and the list of tuples of Functional Dependencies
 
+    Goal: If a determinant has a dependent that is also a determinant with
+    its own dependents, a transitive connection is found
+    """
+    transitive_dependencies = {}
+    intermediate_attrs = {}
+
+    # For each attribute and it's closure set
     for key, closure_set in closure_dict.items():
+        # For each attribute in that closure set
         for attribute in closure_set:
+            # If the attribute in the set is a determinant and isn't itself
+            # (As closure of A will include A itself)
             if (attribute in determinants) & (attribute != key):
+                # For deter and dep list in FD_list
                 for determinant, dependent in FD_list:
+                    # Add the intermdiate connection of deter to dep
+                    # to intermediate_attrs
+                    intermediate_attrs[determinant] = dependent
+                    # If the attribute that is both a dep and a deter,
+                    # add the original determinant as the key and
+                    # this attribute's dependents unique set as the value
+                    # to transitive_dependencies
                     if determinant == attribute:
                         if key not in transitive_dependencies:
                             transitive_dependencies[key] = set()
                         for dep in dependent:
                             transitive_dependencies[key].add(dep)
 
+    print("Intermediate Attrs: ", intermediate_attrs)
+
+    # Convert transitive_deps' values from a set to a list
     transitive_dependencies = {key: list(value) for key, value in transitive_dependencies.items()}
 
     print("")
     print("Possible Transitive Dependencies: ", transitive_dependencies)
 
+    # If the transitive_dependencies list length is > 0, a transitive_dep
+    # was found and hence the dataframe does not satisfy 3NF
     if len(transitive_dependencies) > 0:
         satisfies_3nf = False
     else:
         satisfies_3nf = True
 
-    return satisfies_3nf, transitive_dependencies
+    return satisfies_3nf, transitive_dependencies, intermediate_attrs
 
 
 def suggest_candidate_key(relation: set, closure_dict: dict):
+    """
+    Returns a dict of candidate_keys given the relation set and the closure
+    dict.
+    Goal: Candidate Keys are minimal superkeys, hence return a dict with keys
+    with the least amount of attributes that lead to every attribute in the
+    relation
+    """
+
     superkeys = {}
     candidate_keys = {}
 
@@ -175,21 +294,32 @@ def suggest_candidate_key(relation: set, closure_dict: dict):
         if len(closure) == len(relation):
             superkeys[attribute] = closure
 
-    # If no subset of the superkey is superkey, the attribute is a candidate key
+    # If no subset of the superkey is superkey, the attribute is a
+    # candidate key
     for superkey, closure in superkeys.items():
         attrs = superkey.split('/')
 
-        # If there's only one attr, its subset if null, to it's a C key
+        # If there's only one attr, its subset is null, so it's a Candidate key
         if len(attrs) == 1:
             candidate_keys[superkey] = closure
         else:
             combinations = []
-            for r in range(1, len(attrs) + 1):
-                for combo in itertools.combinations(attrs, r):
+            # After seperating the composite attr into its individual attrs,
+            # create a list of its combinations
+            # i.e A|B = [A, B, A/B]
+            for combo_size in range(1, len(attrs) + 1):
+                for combo in itertools.combinations(attrs, combo_size):
+                    # If combo isn't already a superkey, add combo to
+                    # combiations list
                     if str(combo) != str(superkey):
                         combinations.append('/'.join(combo))
+            # making sure the superkey isn't in the combinantions list
             combinations.remove(superkey)
 
+            # If any subset of the superkey's attributes is a superkey itself,
+            # then the largest superkey is not a candidate key
+            # i.e if A|B is a superkey and A is a superkey, then A|B is not a
+            # candidate key
             is_candidate_key = True
             for combo in combinations:
                 if combo in superkeys:
@@ -199,6 +329,7 @@ def suggest_candidate_key(relation: set, closure_dict: dict):
             if is_candidate_key:
                 candidate_keys[superkey] = closure
 
+        # Just so A|B and A/B are considered the same
         for key1 in list(candidate_keys.keys()):
             cand_key1 = key1.replace('|', ',').replace('/', ',')
             for key2 in list(candidate_keys.keys())[1:]:
@@ -211,7 +342,13 @@ def suggest_candidate_key(relation: set, closure_dict: dict):
     return candidate_keys
 
 
-def check_1NF(df: pd.DataFrame) -> bool:
+def check_1NF(df: pd.DataFrame):
+    """
+    Given a dataframe, check to see if it passes the requirments of 1NF:
+    That every cell has one singluar value and that every row in the df
+    is unique.
+    """
+
     # Check to see if every cell value is a single value
     # i.e. can be a int, float, str, but not a list, array, series
     def is_single_value(x):
@@ -229,27 +366,39 @@ def check_1NF(df: pd.DataFrame) -> bool:
 
 
 def decompose_to_2nf(df: pd.DataFrame, possible_partial_dependencies: dict) -> list:
+    """
+    Given a Dataframe and dict of the partial_dependencies found,
+    this function divides the df into seperate dfs to satisfy 2NF:
+    No partial dependencies
+    """
 
     relations = []
 
-    for key, dependent_attr in possible_partial_dependencies.items():            
+    # For key and a list of its dep attrs create a subset of the df
+    # starting with adding the key
+    for key, dependent_attr in possible_partial_dependencies.items():
         subset = df[[key]].drop_duplicates()
+        # Then add each of its deps to the subset df
         for dep in dependent_attr:
             subset[dep] = df[dep]
+        # Add new df to the relations list
         relations.append(subset)
 
     used_dep_list = []
 
+    # Keeping track of the dependencies already added to one subset
     for dep in possible_partial_dependencies.values():
         for _ in dep:
             used_dep_list.append(_)
 
     print("Used_deps: ", used_dep_list)
 
+    # Put the remaining non-key attributes into the next df subset
     remaining_attrs = [col for col in df.columns if col not in used_dep_list]
     remaining_relation = df[remaining_attrs].drop_duplicates()
     relations.append(remaining_relation)
 
+    # remove any duplicate rows from the dataframe
     for i, relation in enumerate(relations):
         relations[i] = relation.drop_duplicates()
 
